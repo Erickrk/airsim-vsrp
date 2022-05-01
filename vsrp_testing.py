44f228da8d901c7a0dc8caa62e39554c78e2d3bf
erick_silva <ericksilvarod@gmail.com>
2022-04-30 22:39:26 -0300

    implemented lidar and movement functions

--- vsrp_testing.py
+++ vsrp_testing.py
@@ -1,7 +1,6 @@
 # Adapted from airsim's hello_car
-# connect to the AirSim simulator
 
-import setup_path
+#import setup_path
 import airsim
 import cv2
 import numpy as np
@@ -15,53 +14,46 @@ client.enableApiControl(True)
 print("API Control enabled: %s" % client.isApiControlEnabled())
 car_controls = airsim.CarControls()
 
-
-for idx in range(5):
+# steering possible values: 1 -> right
+#                          -1 -> left
+#                           0 -> none                         
+def movement(direction, time = 1):
+    if(direction == "forward"):
+        car_controls.throttle = 0.75
+        car_controls.steering = 0
+        client.setCarControls(car_controls)
+        
+    elif(direction == "forward_right"):
+        car_state = client.getCarState()
+        car_controls.throttle = 0.5
+        car_controls.steering = 1
+        client.setCarControls(car_controls)
+        
+    elif(direction == "forward_left"):
+        car_controls.throttle = 0.5
+        car_controls.steering = -1
+        client.setCarControls(car_controls)
+        
+for idx in range(1):
     # get state of the car
     car_state = client.getCarState()
     print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
 
-    # Go forward + steer right
-    # then sleeps so car can drive alone
-    car_controls.throttle = 0.5
-    car_controls.steering = 1
-    client.setCarControls(car_controls)
-    time.sleep(3)  
-
-    # go forward
-    car_controls.throttle = 0.75
-    car_controls.steering = 0
-    client.setCarControls(car_controls)
+    movement("forward_right", 3)
+    time.sleep(3)  #TODO insert this inside the movement function
+    movement("forward", 6)
     time.sleep(6)  
-
-    # Go forward + steer left
-    car_controls.throttle = 0.5
-    car_controls.steering = -1
-    client.setCarControls(car_controls)
+    movement("forward_left", 1)
     time.sleep(1)   
-
-    # go forward
-    car_controls.throttle = 2
-    car_controls.steering = 0
-    client.setCarControls(car_controls)
-    time.sleep(5) 
-
-    # Go forward + steer left
-    car_controls.throttle = 0.5
-    car_controls.steering = -1
-    client.setCarControls(car_controls)
-    # print("Go Forward, steer right")
-    time.sleep(3)   # let car drive a bit
-    car_controls.is_manual_gear = False # change back gear to auto
-    car_controls.manual_gear = 0
-
-    # apply brakes
+    movement("forward", 6)
+    time.sleep(10) 
+    movement("forward_left", 1)
+    time.sleep(3)
+       
+    # Final brake
     car_controls.brake = 1
     client.setCarControls(car_controls)
     time.sleep(3)  
     car_controls.brake = 0 #remove brake
-
-#restore to original state
 client.reset()
 
-client.enableApiControl(False)
